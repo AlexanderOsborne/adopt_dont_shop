e76723e87e99cf7a42a207563cd73ff8a1a06921
awosborn <alanturing@example.coclear>
2020-12-17 15:27:11 -0700

    Add test applications dont affect each other

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -37,5 +37,26 @@ describe 'As a visitor' do
     expect(page).to have_no_button("Reject Adoption")
     expect(page).to have_content("We are sorry to inform you your adoption has been rejected.")
   end
+
+  it 'Approve/Reject on one application doesnt affect other applications' do
+    joe = Application.create!(name: 'Joe', street_address: "12 Broadway", city: "Boulder", state: "CO", zip: 80303)
+    dave = Application.create!(name: 'Joe', street_address: "12 Broadway", city: "Boulder", state: "CO", zip: 80303)
+    
+    shelter = create(:shelter)
+    puppy = create(:pet, shelter: shelter)
+    pet_app = PetApplication.create!(application: joe, pet: puppy, approved: nil)
+    pet_app2 = PetApplication.create!(application: dave, pet: puppy, approved: nil)
+    
+    visit "/admin/applications/#{joe.id}"
+
+    expect(current_path).to eq "/admin/applications/#{joe.id}"
+    click_button "Reject Adoption"
+    pet_app.reload
+  
+    visit "/admin/applications/#{dave.id}"
+  
+    expect(page).to have_button("Approve Adoption")
+    expect(page).to have_button("Reject Adoption")
+  end
 end
 end
