de9cf128190f4eac50b3fa727ed9ed5f4d608d41
awosborn <alanturing@example.coclear>
2020-12-17 20:36:32 -0700

    Add dependent destroy

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
-  has_many :pet_applications
+  has_many :pet_applications, dependent: :destroy
   has_many :pets, through: :pet_applications
 
   validates_presence_of :name, :street_address, :state, :zip, :city
