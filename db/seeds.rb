cfd8081265a22ac777d72f5865e643a1edf7d130
awosborn <alanturing@example.coclear>
2020-12-17 20:34:02 -0700

    Remove factorybot from seed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Pet.destroy_all
-Shelter.destroy_all
 Application.destroy_all
-
-FactoryBot.create_list(:shelter, 4)
-
-Shelter.all.each do |shelter|
-  rand(1..5).times do 
-    shelter.pets.create!(name: Faker::Creature::Dog.name, approximate_age: 2, description: Faker::Company.bs, sex: [:male,:female].sample)
-  end
-end
+Shelter.destroy_all
+Pet.destroy_all
+Application.create!(name: "James Fox", street_address: "2286 Huntington Dr", city: "LHC", state: "AZ", zip: 86403, description: "I WANT HIM!")
+@shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+@shelter2 = Shelter.create!(name: "Silly Shelter", address: "123 Silly Ave", city: "Longmont", state: "CO", zip: 80012)
+@shelter3 = Shelter.create!(name: "Shell Shelter", address: "102 Shelter Dr.", city: "Commerce City", state: "CO", zip: 80022)
+@shelter1.pets.create!(image:"puppies.jpeg", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+@shelter2.pets.create!(image:"puppies.jpeg", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "Zeus", description: "dog", approximate_age: 4, sex: "male")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "pepper", description: "dog", approximate_age: 6, sex: "male")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "tucker", description: "dog", approximate_age: 3, sex: "male")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "Babe", description: "dog", approximate_age: 9, sex: "male")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "Luna", description: "dog", approximate_age: 2, sex: "male")
+@shelter1.pets.create!(image:"puppies.jpeg", name: "Rex", description: "dog", approximate_age: 4, sex: "male")
+@shelter2.pets.create!(image:"puppies.jpeg", name: "Party", description: "cat", approximate_age: 5, sex: "female")
+@shelter2.pets.create!(image:"puppies.jpeg", name: "Chip", description: "cat", approximate_age: 1, sex: "female")
+@shelter2.pets.create!(image:"puppies.jpeg", name: "Asshole", description: "cat", approximate_age: 8, sex: "female")
+@shelter2.pets.create!(image:"puppies.jpeg", name: "Deus", description: "cat", approximate_age: 3, sex: "female")
